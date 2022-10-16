0676067c2641854008d79e6dae254e52d885442f
Oscar Santiago <73810683+oscarLS-11@users.noreply.github.com>
2022-10-15 22:44:16 -0500

    1. change oscar

---
+++ app/Http/Controllers/TreatmentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TreatmentController extends Controller
+{
+    //
+    public function index(){
+        return "Hola desde la vista Treatment";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('plant', 'App\Http\Controllers\PlantController@index');
+Route::get('staff', 'App\Http\Controllers\StaffController@index');
+Route::get('user', 'App\Http\Controllers\UserController@index');
+Route::get('position', 'App\Http\Controllers\PositionController@index');
+Route::get('departament', 'App\Http\Controllers\DepartamentController@index');
+Route::get('material', 'App\Http\Controllers\MaterialController@index');
+Route::get('color', 'App\Http\Controllers\ColorController@index');
+Route::get('treatment', 'App\Http\Controllers\TreatmentController@index');
+Route::get('chemical', 'App\Http\Controllers\ChemicalController@index');
+Route::get('customer', 'App\Http\Controllers\CustomerController@index');
+Route::get('productionorder', 'App\Http\Controllers\ProductionOrderController@index');
