0ed35474fa79b77f2016fd19b452e7887f8174aa
emmanueltejedatejeda <emmanuel.2d.28@gmail.com>
2022-10-16 20:36:12 -0500

    16_10_2022

--- app/Http/Controllers/ChemicalController.php
+++ app/Http/Controllers/ChemicalController.php
@@ -10,4 +10,24 @@ class ChemicalController extends Controller
     public function index(){
         return "Hola desde la vista Chemical";
     }
+    
+
+    /*
+    public function index(){
+        return view('chemical.index');
+    }
+
+    public function store(StoreChemicalRequest $request){
+        $request->validate([
+            'nombre'                => 'required',
+            'fabricante'            => 'required',
+            'descripcion'           => 'required',
+            'cantidadDisponible'    => 'required',
+        ]);
+        return redirect()->route('chemical.index');
+    }
+
+    
+    */ 
 }
+
